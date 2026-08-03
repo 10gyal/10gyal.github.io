f4d9b86a4390b96b1d94c924f91042ce0c9665d9
10gyal <53687328+10gyal@users.noreply.github.com>
2026-08-03 13:13:09 +0000

    Deploying to gh-pages from @ 10gyal/10gyal.github.io@07ef40a63379e4a5187bcb8b2f3b78223b0de73f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "Professional experience, research, projects, and awards.",
+          description: "Professional experience, research, and awards.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
